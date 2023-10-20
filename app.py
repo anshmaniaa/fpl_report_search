d2c24e09199b24d6b9d28ed33804d329f09393c4
anshmaniaa <anshuman.bhadauria@icloud.com>
2023-10-20 20:24:00 +0530

    fix

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
+from langchain.memory import ConversationBufferMemory
 
 st.title("Report Search")
 
 upload_file = st.file_uploader
+
+memory = ConversationBufferMemory(memory_key="chat_history")
 
 agent = create_csv_agent(
     ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
     "reports.csv",
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,
+    memory=memory,
 )
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
